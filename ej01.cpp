df2fe6d32ef7c7e67425020423c7076b1aacb838
Uriusk <43259329+Uriusk@users.noreply.github.com>
2021-07-23 06:32:31 -0300

    Add files via upload

---
+++ ej01.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+
+
+main(){
+    float a=0;float b=0;float c=0;float d=0;float e=0;float f=0;float g=0;float h=0;float i=0;float j=0;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> a;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> b;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> c;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> d;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> e;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> f;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> g;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> h;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> i;
+    std::cout << "Ingrese un nota " << std::endl;
+    std::cin >> j;
+    std::cout << "La suma de las notas es: " << a+b+c+d+e+f+g+h+i+j << std::endl;
+    std::cout << "El promedio de las notas es: " << (a+b+c+d+e+f+g+h+i+j)/10 << std::endl;
+    return 0;
+}
+
